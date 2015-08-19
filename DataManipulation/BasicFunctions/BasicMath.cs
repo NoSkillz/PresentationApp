d73848c8c60b2731c25807abbadcff5204fdeca5
Andrei Serban <noskillz05@gmail.com>
2015-08-19 20:43:41 +0300

    add double and float to addition

--- DataManipulation/BasicFunctions/BasicMath.cs
+++ DataManipulation/BasicFunctions/BasicMath.cs
@@ -8,7 +8,22 @@ namespace DataManipulation.BasicFunctions
 {
     public static class BasicMath
     {
+        // Integer addition
         public static int Addition(int firstNumber, int secondNumber)
+        {
+            var result = firstNumber + secondNumber;
+            return result;
+        }
+
+        // Double addition
+        public static double Addition(double firstNumber, double secondNumber)
+        {
+            var result = firstNumber + secondNumber;
+            return result;
+        }
+
+        //Float addition
+        public static float Addition(float firstNumber, float secondNumber)
         {
             var result = firstNumber + secondNumber;
             return result;
