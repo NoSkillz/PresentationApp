02923af6934b2d24d3a0d6c37a9795bc16460313
Andrei Serban <noskillz05@gmail.com>
2015-08-27 08:49:17 +0300

    woo-hoo for datepicker!

--- PresentationApp/Controllers/PresentationsController.cs
+++ PresentationApp/Controllers/PresentationsController.cs
@@ -47,7 +47,7 @@ namespace PresentationApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Description,Date")] Presentation presentation)
+        public ActionResult Create([Bind(Include = "Name,Description,Date")] Presentation presentation)
         {
             if (ModelState.IsValid)
             {
