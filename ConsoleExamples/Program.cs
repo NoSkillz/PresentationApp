43db8b2450f0210539d4d7641742a3ef24428ec2
Andrei Serban <noskillz05@gmail.com>
2015-08-30 23:39:27 +0300

    stuff

--- ConsoleExamples/Program.cs
+++ ConsoleExamples/Program.cs
@@ -10,13 +10,23 @@ namespace ConsoleExamples
     {
         static void Main()
         {
-            int Int;
-            float Float;
-            double Double;
-            decimal Decimal;
+            string values = string.Format("Int max: {0}\nInt min: {1}\nDouble max: {2}\nDouble min: {3}\nFloat max: {4}\nFloat min: {5}\nDecimalMax: {6}\nDecimal min: {7}\n",
+                int    .MaxValue,
+                int    .MinValue,
+                double .MaxValue,
+                double .MinValue,
+                float  .MaxValue,
+                float  .MinValue,
+                decimal.MaxValue,
+                decimal.MinValue);
+            Console.Write(values);
 
-            var firstNumber = Console.ReadLine();
-            var secondNumber = Console.ReadLine();
+            // Be specific
+            Console.WriteLine("First number: ");
+            var firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second number: ");
+            var secondNumber = int.Parse(Console.ReadLine());
 
             var result = firstNumber + secondNumber;
 
