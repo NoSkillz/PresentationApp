901bfba6c1aa9aa8949aeb53bf3475073569c45d
Andrei Serban <noskillz05@gmail.com>
2015-08-30 17:12:03 +0300

    simple console

---
+++ ConsoleExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Int;
+            float Float;
+            double Double;
+            decimal Decimal;
+
+            var firstNumber = Console.ReadLine();
+            var secondNumber = Console.ReadLine();
+
+            var result = firstNumber + secondNumber;
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
